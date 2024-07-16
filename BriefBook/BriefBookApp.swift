87de21e0f9082fa2673a46d5a173119c088f5ce2
Daniil Kucherenko <daniil.ukraine@gmail.com>
2024-07-16 19:57:20 +0300

    Initial commit of the proj files

---
+++ BriefBook/BriefBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  BriefBookApp.swift
+//  BriefBook
+//
+//  Created by Daniil on 16.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct BriefBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
