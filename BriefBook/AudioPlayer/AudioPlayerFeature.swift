d86e2fa8a8458e5407c9a94b85a3e17b657c4c38
Daniil Kucherenko <daniil.ukraine@gmail.com>
2024-07-22 01:47:54 +0300

    Add a feature and a view for controlling the player

---
+++ BriefBook/AudioPlayer/AudioPlayerFeature.swift
@@ -0,0 +1,138 @@
+//
+//  AudioPlayerFeature.swift
+//  BriefBook
+//
+//  Created by Daniil on 19.07.2024.
+//
+
+import Foundation
+import ComposableArchitecture
+import AVFAudio
+
+@Reducer
+struct AudioPlayerFeature {
+    @ObservableState
+    struct State {
+        var isPlaying = false
+        var totalTime: TimeInterval = 0.0
+        var currentTime: TimeInterval = 0.0
+        var speed: Speed = .normal
+        var player: AVAudioPlayer?
+        
+        enum Speed: Float {
+            case slow = 0.5
+            case normal = 1
+            case fast = 1.5
+            
+            var formatted: String {
+                switch self {
+                case .slow:
+                    return "0.5"
+                case .normal:
+                    return "1"
+                case .fast:
+                    return "1.5"
+                }
+            }
+        }
+    }
+    
+    enum Action {
+        case setupPlayer(URL)
+        case playerInitialized(Result<AVAudioPlayer, Error>)
+        
+        case totalTimeChanged(TimeInterval)
+        case timeStampChanged(TimeInterval)
+        case updateTimeStampProgress
+        
+        case speedChanged
+        
+        case previousTrackTapped
+        case rewindTapped
+        case playPauseTapped
+        case forwardTapped
+        case nextTrackTapped
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .setupPlayer(let url):
+                return .run { send in
+                    do {
+                        let player = try AVAudioPlayer(contentsOf: url)
+                        player.prepareToPlay()
+                        player.enableRate = true
+                        await send(.playerInitialized(.success(player)))
+                    } catch {
+                        await send(.playerInitialized(.failure(error)))
+                    }
+                }
+            case .playerInitialized(let result):
+                switch result {
+                case .success(let player):
+                    state.player = player
+                    state.totalTime = player.duration
+                case .failure(let error):
+                    print("Error loading audio: \(error)")
+                }
+                return .none
+                
+            case .totalTimeChanged(let totalTime):
+                state.totalTime = totalTime
+                return .none
+            case .timeStampChanged(let newTimeStamp):
+                state.currentTime = newTimeStamp
+                print("Timestamp changed to: \(newTimeStamp)")
+                return .none
+            case .updateTimeStampProgress:
+                state.currentTime = state.player?.currentTime ?? 0
+                return .none
+                
+            case .speedChanged:
+                state.speed = nextSpeed(currentSpeed: state.speed)
+                state.player?.rate = state.speed.rawValue
+                return .none
+            
+            case .playPauseTapped:
+                if state.isPlaying {
+                    state.player?.pause()
+                } else {
+                    state.player?.play()
+                }
+                state.isPlaying.toggle()
+                return .none
+            case .previousTrackTapped:
+                print("Prev track clicked")
+                return .none
+            case .rewindTapped:
+                // 5 sec backwards
+                state.player?.currentTime -= 5
+                state.currentTime = state.player?.currentTime ?? 0
+                return .none
+            case .forwardTapped:
+                // 10 sec forward
+                state.player?.currentTime += 10
+                state.currentTime = state.player?.currentTime ?? 0
+                return .none
+            case .nextTrackTapped:
+                // switch to the next track
+                print("Next track clicked")
+                return .none
+            }
+        }
+    }
+    
+    // MARK: - Function for getting the next speed for audio
+    private func nextSpeed(currentSpeed: State.Speed) -> State.Speed {
+        switch currentSpeed {
+        case .slow:
+            return .normal
+        case .normal:
+            return .fast
+        case .fast:
+            return .slow
+        }
+    }
+}
+
