4cfa0aac1a6ba8779b18185e2f5c3afc4242d8ac
Daniil Kucherenko <daniil.ukraine@gmail.com>
2024-07-23 22:35:12 +0300

    Add a general feature for app

---
+++ BriefBook/AppFeature.swift
@@ -0,0 +1,95 @@
+//
+//  AppFeature.swift
+//  BriefBook
+//
+//  Created by Daniil on 23.07.2024.
+//
+
+import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct AppFeature {
+    struct State: Equatable {
+        var bookInfoState = BookInformationFeature.State()
+        var audioState = AudioPlayerFeature.State()
+        var tabsState = TabsFeature.State()
+    }
+    
+    enum Action {
+        case bookInfoAction(BookInformationFeature.Action)
+        case audioAction(AudioPlayerFeature.Action)
+        case tabAction(TabsFeature.Action)
+    }
+    
+    var body: some ReducerOf<Self> {
+        Scope(state: \.bookInfoState, action: \.bookInfoAction) {
+            BookInformationFeature()
+        }
+        Scope(state: \.audioState, action: \.audioAction) {
+            AudioPlayerFeature()
+        }
+        Scope(state: \.tabsState, action: \.tabAction) {
+            TabsFeature()
+        }
+        
+        Reduce { state, action in
+//            switch action {
+//            case .audioAction(let audioAction):
+//                switch audioAction {
+//                case .previousTrackTapped:
+//                    return .send(.bookInfoAction(.keyPointMoveBackward))
+//                case .nextTrackTapped:
+//                    return .send(.bookInfoAction(.keyPointMoveForward))
+//                default:
+//                    return .none
+//                }
+//                
+//            case .tabAction(let tabAction):
+//                return .none
+//                
+//            case .bookInfoAction(let bookInfoAction):
+//                switch action {
+//                case .setInitialValues(let book):
+//                    state.bookInfoState = BookInformationFeature.State(
+//                        bookImage: "exampleImageName", // Update with actual logic
+//                        keypoint: 1,
+//                        keyPoints: ["Point 1", "Point 2", "Point 3"],
+//                        chapterTitle: "Example Chapter Title"
+//                    )
+//                    return .none
+//                default:
+//                    return .none
+//                }
+//            }
+            switch action {
+                    case .audioAction(let audioAction):
+                        switch audioAction {
+                        case .previousTrackTapped:
+                            return .send(.bookInfoAction(.keyPointMoveBackward))
+                        case .nextTrackTapped:
+                            return .send(.bookInfoAction(.keyPointMoveForward))
+                        default:
+                            return .none
+                        }
+                        
+                    case .tabAction(let tabAction):
+                        return .none
+                        
+                    case .bookInfoAction(let bookInfoAction):
+                        switch bookInfoAction {
+                        case .setInitialValues(let book):
+                            state.bookInfoState = BookInformationFeature.State(
+                                keypoint: 1,
+                                keyPoints: book.keyPoints,
+                                chapterTitle: book.chapters.first!,
+                                chapters: book.chapters
+                            )
+                            return .none
+                        default:
+                            return .none
+                        }
+                    }
+        }
+    }
+}
