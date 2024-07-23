3a79fa4d641c2d95d5acbdb2d6bea6ee8ecedac9
Daniil Kucherenko <daniil.ukraine@gmail.com>
2024-07-23 22:27:20 +0300

    Make a separate view and feature for book info

---
+++ BriefBook/BookInformationView/BookInformationFeature.swift
@@ -0,0 +1,57 @@
+//
+//  BookInformationFeature.swift
+//  BriefBook
+//
+//  Created by Daniil on 23.07.2024.
+//
+
+import SwiftUI
+import ComposableArchitecture
+import Kingfisher
+
+@Reducer
+struct BookInformationFeature {
+    @ObservableState
+    struct State: Equatable {
+        var keypoint = 1
+        var keyPoints: [String: TimeInterval] = [:]
+        var chapterTitle: String = ""
+        var chapters: [String] = []
+    }
+    
+    enum Action {
+        case setInitialValues(book: Book)
+        case keyPointMoveForward
+        case keyPointMoveBackward
+        case chapterTitleChanged(String)
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .setInitialValues(let book):
+                state.keypoint = 1
+                state.chapters = book.chapters
+                state.chapterTitle = book.chapters.first!
+                state.keyPoints = book.keyPoints
+                return .none
+            case .keyPointMoveForward:
+                if state.keypoint < state.keyPoints.count {
+                    state.keypoint += 1
+                    state.chapterTitle = state.chapters[state.keypoint - 1]
+                }
+                return .none
+            case .keyPointMoveBackward:
+                if state.keypoint > 1 {
+                    state.keypoint -= 1
+                    state.chapterTitle = state.chapters[state.keypoint - 1]
+                }
+                return .none
+                
+            case .chapterTitleChanged(let title):
+                state.chapterTitle = title
+                return .none
+            }
+        }
+    }
+}
---
+++ BriefBook/BookInformationView/BookInformationView.swift
@@ -0,0 +1,39 @@
+//
+//  BookInformationView.swift
+//  BriefBook
+//
+//  Created by Daniil on 22.07.2024.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct BookInformationView: View {
+    let book: Book
+    let store: StoreOf<BookInformationFeature>
+    
+    var body: some View {
+        VStack {
+            Image(.bookPhotoPlaceholder)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 240)
+            Text("KEY POINT \(store.keypoint) OF \(store.keyPoints.count)")
+                .font(.subheadline)
+                .bold()
+                .padding(.top, 30)
+                .foregroundStyle(.gray)
+            Text(store.chapterTitle)
+                .font(.callout)
+                .multilineTextAlignment(.center)
+                .padding(.top, 5)
+                .padding(.horizontal, 25)
+        }
+    }
+}
+
+#Preview {
+    BookInformationView(book: .mock, store: Store(initialState: BookInformationFeature.State()) {
+        BookInformationFeature()
+    })
+}
